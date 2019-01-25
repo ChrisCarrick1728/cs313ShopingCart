b938dcd733592fa03cb7631588577aaaa062a10b
Chris Carrick-2 <car17059@byui.edu>
2019-01-24 22:58:00 -0700

    bug fixes

--- components/browse.php
+++ components/browse.php
@@ -3,6 +3,9 @@
     <h3><?php echo $k['name']; ?></h3>
     <div class="imgContainer">
       <img src="<?php echo $k['imgURL']; ?>" alt="">
+      <div class="priceBackground">
+        <!-- empty div for dark background -->
+      </div>
       <p class="price">$ <?php echo $k['price']; ?></p>
       <p class="addToCart"></p>
       <input type="button" class="addToCartButton" name="button" value="+" id="<?php echo "add_{$k['id']}"; ?>">
--- php/setupCart.php
+++ php/setupCart.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$filename = "./items/cart.txt";
+$filename = "./items/cart.json";
 if (!isset($_SESSION['cart'])) {
   $_SESSION['cart'] = file_get_contents($filename);
   $_SESSION['numItems'] = '0';
